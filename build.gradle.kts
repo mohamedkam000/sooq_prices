aef89dc2b20c3897102ad32cb6f10dc317636745
Muhammad <mohamedkam000@gmail.com>
2025-08-04 23:49:59 +0200

    Build file fixup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,6 @@
 plugins {
-    id("com.android.application") version "8.4.0" apply false
-    kotlin("android") version "2.0.0" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.android.library) apply false
 }
