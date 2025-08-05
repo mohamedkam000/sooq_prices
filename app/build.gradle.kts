e4223ff9342e5feeb0c26f4aba8f9646fb880a6b
Muhammad <mohamedkam000@gmail.com>
2025-08-05 12:30:21 +0200

    How is life with compiler options?

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,9 +35,6 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    compilerOptions {
-        jvmTarget.set(JavaVersion.VERSION_17)
-    }
 }
 
 dependencies {
