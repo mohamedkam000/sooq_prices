26ceedc53a8678b0c9238713e2bc5e63d3f68034
Muhammad <mohamedkam000@gmail.com>
2025-08-06 19:57:37 +0200

    add ui tooling

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,7 +53,8 @@ android {
 dependencies {
     implementation(platform("androidx.compose:compose-bom:2025.07.00"))
     implementation("androidx.compose.ui:ui:1.9.0-rc01")
-//    implementation("androidx.compose.ui:ui-tooling-preview:1.9.0-rc01")
+    implementation("androidx.compose.ui:ui-tooling:1.9.0-rc01")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.9.0-rc01")
     implementation("androidx.activity:activity-compose:1.12.0-alpha05")
 //    implementation("androidx.compose.runtime:runtime:1.9.0-rc01")
     implementation("androidx.compose.material3:material3:1.5.0-alpha01")
