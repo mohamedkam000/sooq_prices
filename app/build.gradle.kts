f6cdfdf07996a1fbf32da07d54714a29caeac5f7
Muhammad <mohamedkam000@gmail.com>
2025-08-06 17:40:31 +0200

    Test compile 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,14 +41,22 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.13"
+    }
+}
+
+kotlin {
+    jvmToolchain(17)
 }
 
 dependencies {
-    implementation(platform("androidx.compose:compose-bom:2024.06.00"))
+    implementation(platform("androidx.compose:compose-bom:2025.07.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.activity:activity-compose")
     implementation("androidx.compose.runtime:runtime")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material3:material3-expressive")
     implementation("androidx.navigation:navigation-compose:2.9.2")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
