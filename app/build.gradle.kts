be1a9394d76c99360db9e6adbc13929cb3581cd0
Muhammad <mohamedkam000@gmail.com>
2025-08-06 18:38:32 +0200

    A new beginning

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("org.jetbrains.kotlin.plugin.compose")
 }
 
 android {
@@ -36,30 +35,29 @@ android {
         compose = true
     }
 
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.15"
     }
 
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.13"
+    kotlinOptions {
+        jvmTarget = "19"
     }
 }
 
 kotlin {
-    jvmToolchain(17)
+    jvmToolchain(19)
 }
 
 dependencies {
     implementation(platform("androidx.compose:compose-bom:2025.07.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.activity:activity-compose")
-    implementation("androidx.compose.runtime:runtime")
+    implementation("androidx.compose.ui:ui:1.9.0-rc01")
+//    implementation("androidx.compose.ui:ui-tooling-preview:1.9.0-rc01")
+    implementation("androidx.activity:activity-compose:1.12.0-alpha05")
+//    implementation("androidx.compose.runtime:runtime:1.9.0-rc01")
     implementation("androidx.compose.material3:material3:1.5.0-alpha01")
-    implementation("androidx.navigation:navigation-compose:2.9.2")
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.activity:activity-ktx")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.0")
+//    implementation("androidx.navigation:navigation-compose:2.9.2")
+//    implementation("androidx.appcompat:appcompat:1.7.1")
+//    implementation("com.google.android.material:material:1.14.0-alpha03")
+//    implementation("androidx.activity:activity-ktx:1.12.0-alpha05")
+//    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.10.0-alpha01")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application") version "8.13.0-alpha02" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.23" apply false
-    id("com.android.library") version "8.13.0-alpha02" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.2.0" apply false
+    id("com.android.application") version "8.13.0-alpha03" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.20-Beta2" apply false
+    id("com.android.library") version "8.13.0-alpha03" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.20-Beta2" apply false
 }
