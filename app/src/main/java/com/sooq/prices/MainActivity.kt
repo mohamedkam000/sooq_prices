db8de151c91054edfd869a7a1b14ddcbffc0e4b6
Muhammad <mohamedkam000@gmail.com>
2025-08-06 19:40:26 +0200

    theme fix 3

--- app/src/main/java/com/sooq/prices/MainActivity.kt
+++ app/src/main/java/com/sooq/prices/MainActivity.kt
@@ -3,26 +3,16 @@ package com.sooq.prices
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.sooq.prices.ui.theme.SooqPricesTheme
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SooqPricesTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    WelcomeScreen()
-                }
+            MaterialTheme {
+                WelcomeScreen()
             }
         }
     }
@@ -30,13 +20,13 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun WelcomeScreen() {
-    Box(
-        contentAlignment = Alignment.Center,
-        modifier = Modifier.fillMaxSize()
-    ) {
-        Text(
-            text = "Welcome to the App!",
-            fontSize = 24.sp
-        )
+    Text("Welcome to Sooq Prices!")
+}
+
+@Preview
+@Composable
+fun PreviewWelcome() {
+    MaterialTheme {
+        WelcomeScreen()
     }
 }
