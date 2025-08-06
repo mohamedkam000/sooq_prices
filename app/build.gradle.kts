415fa1aa33ee94ea3251f972281a3ae9478f8c39
Muhammad <mohamedkam000@gmail.com>
2025-08-06 19:33:07 +0200

    theme fix 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,7 +56,7 @@ dependencies {
 //    implementation("androidx.compose.ui:ui-tooling-preview:1.9.0-rc01")
     implementation("androidx.activity:activity-compose:1.12.0-alpha05")
 //    implementation("androidx.compose.runtime:runtime:1.9.0-rc01")
-    implementation("androidx.compose.material3:material3:1.5.0-alpha01")
+    implementation("androidx.compose.material3:material3")
 //    implementation("androidx.navigation:navigation-compose:2.9.2")
 //    implementation("androidx.appcompat:appcompat:1.7.1")
 //    implementation("com.google.android.material:material:1.14.0-alpha03")
