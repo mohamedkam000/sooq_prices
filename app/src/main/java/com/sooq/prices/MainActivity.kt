347ccd52e276df49a58c7400c3784f2f91087c8f
Muhammad <mohamedkam000@gmail.com>
2025-08-07 12:36:04 +0200

    add missing experimentalmaterial3api

--- app/src/main/java/com/sooq/prices/MainActivity.kt
+++ app/src/main/java/com/sooq/prices/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
