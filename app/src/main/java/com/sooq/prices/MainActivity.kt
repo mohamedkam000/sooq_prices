16a92752b7d8fc014f94128bf3ddb5717515f984
Muhammad <mohamedkam000@gmail.com>
2025-08-08 09:23:48 +0200

    let's hope lerp is fixed

--- app/src/main/java/com/sooq/prices/MainActivity.kt
+++ app/src/main/java/com/sooq/prices/MainActivity.kt
@@ -37,7 +37,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.ui.graphics.lerp
 import androidx.compose.foundation.Image
-import androidx.compose.animation.core.lerp
+//import androidx.compose.animation.core.lerp
 import androidx.compose.ui.unit.lerp
 
 class MainActivity : ComponentActivity() {
