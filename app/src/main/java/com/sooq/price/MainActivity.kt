8e7ae138981a51107799751b9691f66254db79e0
Muhammad <mohamedkam000@gmail.com>
2025-08-09 12:20:58 +0200

    test 4

--- app/src/main/java/com/sooq/price/MainActivity.kt
+++ app/src/main/java/com/sooq/price/MainActivity.kt
@@ -6,20 +6,21 @@ import androidx.activity.compose.setContent
 import androidx.core.view.WindowCompat
 import com.sooq.price.ui.navigation.AppNavigation
 
-override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    WindowCompat.setDecorFitsSystemWindows(window, false)
-    window.statusBarColor = android.graphics.Color.TRANSPARENT
-    window.navigationBarColor = android.graphics.Color.TRANSPARENT
-    val windowInsetsController = WindowCompat.getInsetsController(window, window.decorView)
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+            window.statusBarColor = android.graphics.Color.TRANSPARENT
+            window.navigationBarColor = android.graphics.Color.TRANSPARENT
+            val windowInsetsController = WindowCompat.getInsetsController(window, window.decorView)
+        
+            val isDarkTheme = (resources.configuration.uiMode and
+                    android.content.res.Configuration.UI_MODE_NIGHT_MASK) == android.content.res.Configuration.UI_MODE_NIGHT_YES
 
-    val isDarkTheme = (resources.configuration.uiMode and
-            android.content.res.Configuration.UI_MODE_NIGHT_MASK) == android.content.res.Configuration.UI_MODE_NIGHT_YES
-
-    windowInsetsController.isAppearanceLightStatusBars = !isDarkTheme
-    windowInsetsController.isAppearanceLightNavigationBars = true
-
-    setContent {
-        AppNavigation()
+            windowInsetsController.isAppearanceLightStatusBars = !isDarkTheme
+            windowInsetsController.isAppearanceLightNavigationBars = true
+        setContent {
+            AppNavigation()
+        }
     }
 }
