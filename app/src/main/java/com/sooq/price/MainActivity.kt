0074b742272cfd8617aa79c17b7328f1a179fe97
Muhammad <mohamedkam000@gmail.com>
2025-08-09 09:13:24 +0200

    test appintro 2

--- app/src/main/java/com/sooq/price/MainActivity.kt
+++ app/src/main/java/com/sooq/price/MainActivity.kt
@@ -4,9 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.runtime.SideEffect
-import com.google.accompanist.systemuicontroller.rememberSystemUiController
+//import androidx.compose.material3.MaterialTheme
+//import androidx.compose.runtime.SideEffect
+//import androidx.compose.ui.graphics.luminance
+//import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.sooq.price.ui.navigation.AppNavigation
 
 class MainActivity : ComponentActivity() {
@@ -18,7 +19,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             AppNavigation()
 
-            val systemUiController = rememberSystemUiController()
+/*            val systemUiController = rememberSystemUiController()
             val backgroundColor = MaterialTheme.colorScheme.surface
             val darkIcons = backgroundColor.luminance() > 0.5f
 
@@ -31,7 +32,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.surface,
                     darkIcons = darkIcons
                 )
-            }
+            }*/
         }
     }
 }
