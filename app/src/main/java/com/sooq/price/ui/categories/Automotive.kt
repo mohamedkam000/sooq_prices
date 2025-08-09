5151a54053ce7f28a592acf6764bef5330af0ec1
Muhammad <mohamedkam000@gmail.com>
2025-08-09 18:56:52 +0200

    i just understood how imports work

--- app/src/main/java/com/sooq/price/ui/categories/Automotive.kt
+++ app/src/main/java/com/sooq/price/ui/categories/Automotive.kt
@@ -6,7 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.core.view.WindowCompat
 import androidx.core.graphics.ColorUtils
 import com.sooq.price.R
-import com.sooq.price.ui.navigation.Navigation
+import com.sooq.price.ui.navigation.*
 
 // Material 3
 import androidx.compose.material3.*
