24eaf6104b12be8b0039a595e4f7396592d28527
Muhammad <mohamedkam000@gmail.com>
2025-08-10 10:03:46 +0200

    test ui 5

--- app/src/main/java/com/sooq/price/ui/theme/Theme.kt
+++ app/src/main/java/com/sooq/price/ui/theme/Theme.kt
@@ -15,6 +15,7 @@ import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.material3.Typography
 
 private val DarkColorScheme = darkColorScheme(
     primary = PRIMARY,
