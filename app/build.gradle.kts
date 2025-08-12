d3606f2958e8b06eeaf487e8fff0691db4df23ca
Muhammad <mohamedkam000@gmail.com>
2025-08-12 14:12:06 +0200

    test intro update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,6 +72,6 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.10.0-alpha01")
 //    implementation("com.google.accompanist:accompanist-systemuicontroller:0.36.0")
     implementation("com.airbnb.android:lottie-compose:6.6.7")
-    implementation("com.airbnb.android:lottie:6.6.7")
+//    implementation("com.airbnb.android:lottie:6.6.7")
     implementation("com.github.spotbugs:spotbugs-annotations:4.9.3")
 }
