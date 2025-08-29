6b6215de3c8753040528b4988e101a03430c72d2
Muhammad Kamal <mohamedkam000@gmail.com>
2025-08-29 15:26:58 +0100

    Update prices.js

--- docs/prices.js
+++ docs/prices.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const url = "https://raw.githubusercontent.com/your-username/your-repo/main/data.json"; // change to your link
+  const url = "https://raw.githubusercontent.com/mohamedkam000/prices/main/data.json";
 
   fetch(url)
     .then(res => res.json())
@@ -7,6 +7,11 @@ document.addEventListener("DOMContentLoaded", () => {
       // Update prices
       document.getElementById("tomatoes_b").textContent = data.tomatoes_b + " SDG";
       document.getElementById("potatoes_k").textContent = data.potatoes_k + " SDG";
+      document.getElementById("apple_1").textContent = data.tomatoes_b + " SDG";
+      document.getElementById("orange_100").textContent = data.potatoes_k + " SDG";
+      document.getElementById("watermelon").textContent = data.tomatoes_b + " SDG";
+      document.getElementById("grape_k").textContent = data.potatoes_k + " SDG";
+      
     })
     .catch(err => {
       console.error("Error loading data:", err);
