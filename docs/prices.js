4ead1a1c2bb44516ea5a6ffc3166ab674138969f
Muhammad Kamal <mohamedkam000@gmail.com>
2025-08-29 15:37:35 +0100

    Update prices.js

--- docs/prices.js
+++ docs/prices.js
@@ -7,10 +7,10 @@ document.addEventListener("DOMContentLoaded", () => {
       // Update prices
       document.getElementById("tomatoes_b").textContent = data.tomatoes_b + " SDG";
       document.getElementById("potatoes_k").textContent = data.potatoes_k + " SDG";
-      document.getElementById("apple_1").textContent = data.tomatoes_b + " SDG";
-      document.getElementById("orange_100").textContent = data.potatoes_k + " SDG";
-      document.getElementById("watermelon").textContent = data.tomatoes_b + " SDG";
-      document.getElementById("grape_k").textContent = data.potatoes_k + " SDG";
+      document.getElementById("apple_1").textContent = data.apple_1 + " SDG";
+      document.getElementById("orange_100").textContent = data.orange_100 + " SDG";
+      document.getElementById("watermelon").textContent = data.watermelon + " SDG";
+      document.getElementById("grape_k").textContent = data.grape_k + " SDG";
       
     })
     .catch(err => {
