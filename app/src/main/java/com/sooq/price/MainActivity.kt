ff4da8f81758e03412784048cdf7f0862f505262
Muhammad <mohamedkam000@gmail.com>
2025-11-03 15:18:52 +0000

    test

--- app/src/main/java/com/sooq/price/MainActivity.kt
+++ app/src/main/java/com/sooq/price/MainActivity.kt
@@ -71,53 +71,28 @@ fun HomeScreen(navController: NavHostController) {
 
     val dynamicColorScheme = dynamicLightColorScheme(LocalContext.current)
 
-    // Box(
-//         modifier = Modifier
-//             .fillMaxSize()
-//             .background(dynamicColorScheme.background)
-//             .blur(20.dp)
-//             .padding(top = WindowInsets.statusBars.asPaddingValues().calculateTopPadding())
-//     ) {
-//         Column(modifier = Modifier.fillMaxSize()) {
-//             Text(
-//                 "Explore",
-//                 fontSize = 28.sp,
-//                 fontWeight = FontWeight.Bold,
-//                 modifier = Modifier.padding(16.dp)
-//             )
-// 
-//             LazyColumn(
-//                 modifier = Modifier.fillMaxSize(),
-//                 verticalArrangement = Arrangement.spacedBy(16.dp),
-//                 contentPadding = PaddingValues(bottom = 16.dp)
-//             ) {
-//                 items(products) { product ->
-//                     ProductCard(product) {
-//                         navController.navigate("detail/${product.title}/${product.imageUrl}")
-//                     }
-//                 }
-//             }
-//         }
-//     }
-
-    Box(modifier = Modifier.fillMaxSize()) {
-        Box(
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(dynamicColorScheme.primaryContainer)
+            .blur(20.dp)
+    ) {
+        Column(
             modifier = Modifier
                 .fillMaxSize()
-                .background(dynamicColorScheme.background)
-                .blur(20.dp)
-        )
-    
-        Column(modifier = Modifier.fillMaxSize()) {
+                .padding(
+                    top = WindowInsets.statusBars.asPaddingValues().calculateTopPadding(),
+                    bottom = WindowInsets.navigationBars.asPaddingValues().calculateBottomPadding()
+                )
+        ) {
             Text(
                 "Explore",
                 fontSize = 28.sp,
                 fontWeight = FontWeight.Bold,
-                modifier = Modifier.padding(16.dp)
+                modifier = Modifier.padding(horizontal = 16.dp)
             )
     
             LazyColumn(
-                modifier = Modifier.fillMaxSize(),
                 verticalArrangement = Arrangement.spacedBy(16.dp),
                 contentPadding = PaddingValues(bottom = 16.dp)
             ) {
