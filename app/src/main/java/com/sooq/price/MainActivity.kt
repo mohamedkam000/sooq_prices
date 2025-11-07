b7d171bc9e533a8dba3da1ab7ec63d03231eedd8
Muhammad <mohamedkam000@gmail.com>
2025-11-07 13:14:31 +0000

    off

--- app/src/main/java/com/sooq/price/MainActivity.kt
+++ app/src/main/java/com/sooq/price/MainActivity.kt
@@ -21,6 +21,10 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
