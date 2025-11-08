2d86819d4ede9bba6ff048232bc1aaa533ccaa3d
Muhammad <mohamedkam000@gmail.com>
2025-11-08 09:09:07 +0000

    0

--- app/src/main/java/com/sooq/price/MainActivity.kt
+++ app/src/main/java/com/sooq/price/MainActivity.kt
@@ -15,8 +15,7 @@ import androidx.compose.ui.platform.*
 import androidx.compose.ui.input.nestedscroll.*
 import androidx.compose.ui.text.font.*
 import androidx.compose.ui.tooling.preview.*
-import androidx.compose.ui.unit.*
-import androidx.compose.ui.unit.offset
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.draw.*
 import androidx.compose.ui.layout.*
 import androidx.compose.ui.text.style.*
