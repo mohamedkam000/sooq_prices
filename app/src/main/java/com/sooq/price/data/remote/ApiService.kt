4a07f02ab8a647d633dfa37a3ce5e9d6edd8ee41
Muhammad <mohamedkam000@gmail.com>
2025-11-09 18:15:58 +0000

    a FUCKING typo in the url

--- app/src/main/java/com/sooq/price/data/remote/ApiService.kt
+++ app/src/main/java/com/sooq/price/data/remote/ApiService.kt
@@ -4,6 +4,6 @@ import com.sooq.price.data.model.Root
 import retrofit2.http.GET
 
 interface ApiService {
-    @GET("sooq_prices/main/prices.json")
+    @GET("sooq_prices/main/data.json")
     suspend fun fetchData(): Root
 }
