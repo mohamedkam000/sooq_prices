d52f13d0014a66244a6bdf50c12aed49b72ad449
Muhammad <mohamedkam000@gmail.com>
2025-11-10 04:06:54 +0000

    just build already

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    id("com.android.application") version "8.13.0" apply false
+    id("com.android.application") version "9.0.0-alpha14" apply false
     id("org.jetbrains.kotlin.android") version "2.3.0-Beta2" apply false
-    id("com.android.library") version "8.13.0" apply false
+    id("com.android.library") version "9.0.0-alpha14" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.3.0-Beta2" apply false
     id("com.google.dagger.hilt.android") version "2.57.2" apply false
     id("com.google.devtools.ksp") version "2.3.1" apply false
